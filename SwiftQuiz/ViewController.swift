79f8285bae41e5853aca2495164d40bd05907cf9
Jameel Shehadeh <jameelshehadeh@Jameels-MacBook-Pro.local>
2021-11-25 20:00:32 +0400

    Initial Commit

---
+++ SwiftQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Jameel Shehadeh on 11/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
